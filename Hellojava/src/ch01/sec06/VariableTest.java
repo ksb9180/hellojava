d7bf68d677010bd36f7ab53a158cb28cdaad4f81
ksb9180 <ksb9180@naver.com>
2025-03-27 17:44:55 +0900

    2025.03.27

---
+++ Hellojava/src/ch01/sec06/VariableTest.java
@@ -0,0 +1,16 @@
+package ch01.sec06;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte byteValue = 10;
+		float floatValue = 2.5f;
+		double doubleValue = 2.5;
+		
+		byte result = (byte) (byteValue + byteValue);
+		System.out.println(result);
+
+	}
+
+}
