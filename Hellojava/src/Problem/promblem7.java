ba6b64f672c3526c6397c6482f009398101025b0
ksb9180 <ksb9180@naver.com>
2025-03-30 00:37:32 +0900

    2025.03.30

--- Hellojava/src/Problem/promblem7.java
+++ Hellojava/src/Problem/promblem7.java
@@ -9,18 +9,43 @@ public class promblem7 {
 		System.out.println("\t" + "[숫자맞추기게임 시작]");
 		System.out.println("=================================");
 		
-		int randomNumber = (int) (Math.random() * 100) + 1;
-		
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
 		
-		while(true) {
-			if (randomNumber < num) {
-				System.out.println("더 낮게");
+		boolean play = true;
+				
+		while(play) {
+			int randomNumber = (int) (Math.random() * 100) + 1;
+			boolean correct = false;
+			
+			while(!correct) {
+				
+				System.out.print(">>");
+				int num = sc.nextInt();
+				
+				if (randomNumber < num) {
+					System.out.println("더 낮게");
+				} else if (randomNumber > num) {
+					System.out.println("더 높게");
+				} else {
+					System.out.println("맞았습니다.");
+					correct = true;
+				}
 			}
+			
+			System.out.print("게임을 종료하시겠습니까?(y/n) >>");
+			sc.nextLine();
+			String response = sc.nextLine();
+			
+			if (response.equals("y")) {
+				play = false;
+				System.out.println("=================================");
+				System.out.println("\t" + "[숫자맞추기게임 종료]");
+				System.out.println("=================================");
+			}
+			
 		}
 		
-		
+		sc.close();
 		
 	}
 
